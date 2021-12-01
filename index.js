3b280c4529b4bfd79e483dfc6e0266d85c63fa26
joel-monduela-condori <dragonxxxxzzzz@gmail.com>
2021-12-01 17:28:37 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const PORT = process.env.PORT || 3000;
+const DB = process.env.DB || 'mongodb://localhost/blog2021';
+const app = express();
+app.set('view engine','pug');
+app.set('views','./views');
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.get('/',(req, res) => {
+  res.render('index');
+});
+app.listen(PORT,() => {
+  console.log(`server escuchando en puerto ${PORT}`);
+});
