a55b5ebcebfdaea8b106847fd94e36752eae8cc4
DiLacioFAcundo <facundodilacio.esc13@gmail.com>
2021-12-15 17:51:57 -0300

    15/12

--- index.js
+++ index.js
@@ -2,14 +2,33 @@ const express = require('express');
 const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3000;
 const DB = process.env.DB || 'mongodb://localhost/blog2021';
+mongoose.connect(DB, {useNewUrlParser: true })
+  .then(() => console.log('DB Conectada'));
 const app = express();
 app.set('view engine','pug');
 app.set('views','./views');
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+const Post  = require ('./models/Post');
 app.get('/',(req, res) => {
-  res.render('index');
+  Post.find((err, posts) => {
+    res.render('index',{ posts: posts });
+  });
+});
+app.get('/new',(req, res) => {
+  res.render('new_post');
+});
+app.post('/new',(req, res) => {
+  console.log('Datos recibidos:');
+  console.log(req.body);
+  const {title, author, topic, content} = req.body;
+  const post = new Post({ title, author, topic, content });
+  post.save((err,post)=> {
+    console.log('Post creado en la BD');
+    console.log(post);
+    res.redirect('/');
+  });
 });
 app.listen(PORT,() => {
   console.log(`server escuchando en puerto ${PORT}`);
